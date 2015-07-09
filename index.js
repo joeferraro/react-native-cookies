9d0944f601e1eee84182f8e92c730f3365bd09ff
Joe Ferraro <ferraro.joseph@gmail.com>
2015-07-09 08:45:06 -0400

    adds invariant per https://github.com/facebook/react-native/issues/1274#issuecomment-115745110 thanks @nicklockwood

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var NativeModules = require('NativeModules');
+var invariant = require('invariant');
 var RNCookieManagerIOS = NativeModules.RNCookieManagerIOS;
 
 module.exports = {
   set(props, callback) {
+    invariant(
+      NativeModules.RNCookieManagerIOS,
+      'Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project.'
+    );
     RNCookieManagerIOS.set(props, (err, res) => {
       callback(err, res);
     });
   },
   clearAll(callback) {
+    invariant(
+      NativeModules.RNCookieManagerIOS,
+      'Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project.'
+    );
     RNCookieManagerIOS.clearAll((err, res) => {
       callback(err, res);
     });
   },
   getAll(callback) {
+    invariant(
+      NativeModules.RNCookieManagerIOS,
+      'Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project.'
+    );
     RNCookieManagerIOS.getAll((err, res) => {
       callback(err, res);
     });
