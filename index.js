934f67dcb057b359aa5179609519c0abca7e9047
Joe Ferraro <ferraro.joseph@gmail.com>
2017-06-13 14:36:22 -0400

    es6 in index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import {NativeModules, Platform} from 'react-native';
-var invariant = require('invariant');
-var RNCookieManagerIOS = NativeModules.RNCookieManagerIOS;
-var RNCookieManagerAndroid = NativeModules.RNCookieManagerAndroid;
+const invariant = require('invariant');
+const RNCookieManagerIOS = NativeModules.RNCookieManagerIOS;
+const RNCookieManagerAndroid = NativeModules.RNCookieManagerAndroid;
 
-var CookieManager;
+let CookieManager;
+
 if (Platform.OS === 'ios') {
     invariant(RNCookieManagerIOS,
-            'Add RNCookieMangerIOS.h and RNCookieManagerIOS.m to your Xcode project');
+        'Add RNCookieMangerIOS.h and RNCookieManagerIOS.m to your Xcode project');
     CookieManager = RNCookieManagerIOS;
 } else if (Platform.OS === 'android') {
-    invariant(RNCookieManagerAndroid, 'Import libraries to android "rnpm link"');
+    invariant(RNCookieManagerAndroid, 
+        'Import libraries to android "rnpm link"');
     CookieManager = RNCookieManagerAndroid;
 } else {
-    invariant(CookieManager, "Invalid platform");
+    invariant(CookieManager, 'Invalid platform. This library only supports Android and iOS.');
 }
 
 var functions = [
